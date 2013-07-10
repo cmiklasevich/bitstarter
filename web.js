f79c5caca81e8ad667067fb0def1973ca66c090c
John Smith <example@stanford.edu>
2013-07-10 06:22:51 +0000

    fs read added

--- web.js
+++ web.js
@@ -5,6 +5,13 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+
+
+var buffer = new Buffer(100);
+buffer = fs.readFileSync('index.html','utf8');
+console.log (buffer.toString("utf-8", 0, 12));
+
+
   response.send('Hello World 2!');
 });
 
